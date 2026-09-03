5363671374b59faff883b4fc467bcce69319c9e0
natysierra007-afk <natysierra007@gmail.com>
2026-09-03 13:30:32 -0500

    feat: agregar funciones potencia, raiz cuadrada y porcentaje

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,8 @@
 # Calculadora
 
+import math
+
+
 def sumar(a, b):
     return a + b
 
@@ -13,3 +16,15 @@ def dividir(a, b):
     if b == 0:
         raise ValueError("No se puede dividir por cero")
     return a / b
+
+def potencia(base, exponente):
+    return base ** exponente
+ 
+def raiz_cuadrada(a):
+    
+    if a < 0:
+        raise ValueError("No se puede calcular la raíz cuadrada de un número negativo")
+    return math.sqrt(a)
+ 
+def porcentaje(a, porcentaje_valor):
+    return (a * porcentaje_valor) / 100
